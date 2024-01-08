f9a334767848862b093737df253c2e4b6436ef5e
RogueStrix <111405868+RogueStrix@users.noreply.github.com>
2024-01-07 23:18:12 -0600

    Added SpringBoot Project Initializr

---
+++ DylansTimeTracker/DylansTimeTracker/src/main/java/com/roguestrix/DylansTimeTracker/DylansTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.roguestrix.DylansTimeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DylansTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DylansTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ DylansTimeTracker/DylansTimeTracker/src/test/java/com/roguestrix/DylansTimeTracker/DylansTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roguestrix.DylansTimeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DylansTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
